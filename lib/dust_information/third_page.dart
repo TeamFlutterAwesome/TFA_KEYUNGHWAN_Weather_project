983ce432621a41373505bd042d70e0a6d6f1eeb6
keyunghwan <keyunghwan@gmail.com>
2021-09-23 23:24:55 +0900

    디렉토리 정리함

--- lib/dust_information/third_page.dart
+++ lib/dust_information/third_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class third_page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Container();
+    return Container(
+      color: Colors.yellow,
+    );
   }
 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:weather_project_tabview/components/first_page.dart';
-import 'package:weather_project_tabview/components/second_page.dart';
-import 'package:weather_project_tabview/components/third_page.dart';
-import 'package:weather_project_tabview/components/fourth_page.dart';
+import 'package:weather_project_tabview/weather_information/first_page.dart';
+import 'package:weather_project_tabview/weather_image/second_page.dart';
+import 'package:weather_project_tabview/dust_information/third_page.dart';
+import 'package:weather_project_tabview/sea_information/fourth_page.dart';
+import 'package:weather_project_tabview/components/theme.dart';
+import 'package:weather_project_tabview/components/constants.dart';
 
 
 void main() {
@@ -34,7 +36,7 @@ class MyHomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // 1
-    List<String> categories = ["날씨 정보", "레이더", "미세먼지", "바다 날씨"];
+    List<String> categories = ["날씨정보", "날씨영상", "미세먼지", "바다날씨"];
     // 2
     return DefaultTabController(
       // 3
@@ -60,14 +62,14 @@ class MyHomeScreen extends StatelessWidget {
                   ),
                 ),
                 // 5
-                labelColor: Colors.blue,
+                labelColor: Colors.redAccent,
                 // 6
-                unselectedLabelColor: Colors.orange,
-                // labelStyle: textTheme().headline2?.copyWith(
-                //     color: kPrimaryColor, fontWeight: FontWeight.bold),
+                unselectedLabelColor: Colors.grey,
+                labelStyle: textTheme().headline2?.copyWith(
+                    color: kPrimaryColor, fontWeight: FontWeight.bold),
                 // 7
                 indicator: UnderlineTabIndicator(
-                  borderSide: BorderSide(width: 2, color: Colors.blue),
+                  borderSide: BorderSide(width: 4, color: kPrimaryColor),
                 ),
               ),
             ),
