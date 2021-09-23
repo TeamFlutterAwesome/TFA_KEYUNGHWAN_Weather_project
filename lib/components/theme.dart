94654837bc393a80a6426efca1e14686e32e6e2c
keyunghwan <keyunghwan@gmail.com>
2021-09-23 23:26:56 +0900

    사소한 수정

--- lib/components/theme.dart
+++ lib/components/theme.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import '../constants.dart';
+import 'package:weather_project_tabview/components/constants.dart';
 
 ThemeData theme() {
   return ThemeData(
