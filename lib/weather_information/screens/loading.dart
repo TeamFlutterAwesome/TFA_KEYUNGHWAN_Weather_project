f9433b103ac362e23bd6a43765ddee6fe95b1fdc
keyunghwan <keyunghwan@gmail.com>
2021-09-29 19:07:22 +0900

    fetchData 까지 완료

--- lib/weather_information/screens/loading.dart
+++ lib/weather_information/screens/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -7,12 +8,27 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  @override
+  void initState() {
+    // TODO: implement  initState
+    super.initState();
+    getLocation();
+    fetchData();
+  }
+
   void getLocation() async {
+  try {
     Position position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high);
     print(position);
-    print('position');
+  }catch(e){
+    print('There was a problem with the internet connection.');
+  }
+  }
 
+  void fetchData() async {
+    Response response = await get(Uri.parse('https://samples.openweathermap.org/data/2.5/weather?q=London&appid=b1b15e88fa797225412429c1c50c122a1'));
+    print(response.body);
   }
 
   @override
@@ -21,9 +37,7 @@ class _LoadingState extends State<Loading> {
     return Scaffold(
       body: Center(
         child: RaisedButton(
-          onPressed: () {
-            getLocation();
-          },
+          onPressed: null,
           child: Text(
             'Get my location',
             style: TextStyle(color: Colors.white),
