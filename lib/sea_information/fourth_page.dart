fa38ba87c075020212cbbeb14dc25c455b465c53
keyunghwan <keyunghwan@gmail.com>
2021-11-07 16:14:27 +0900

    2,3,4 페이지에 배너 넣으려고 하는데 잘 안됨

--- lib/sea_information/fourth_page.dart
+++ lib/sea_information/fourth_page.dart
@@ -9,39 +9,47 @@ class fourth_page extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      body: WebView(
-        //  initialUrl: 'https://www.weather.go.kr/w/ocean/chart/wave-model.do',
-        initialUrl: 'https://www.dhlottery.co.kr/common.do?method=main',
-        javascriptMode: JavascriptMode.unrestricted,
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              child: WebView(
+                //  initialUrl: 'https://www.weather.go.kr/w/ocean/chart/wave-model.do',
+                initialUrl: 'https://www.dhlottery.co.kr/common.do?method=main',
+                javascriptMode: JavascriptMode.unrestricted,
 
-        onWebViewCreated: (controller) {
-          this.controller = controller;
+                onWebViewCreated: (controller) {
+                  this.controller = controller;
 
-          controller.evaluateJavascript(
-              "document.getElementsById('head')[0].style.display='none';");
-          controller.evaluateJavascript(
-              "document.getElementsByClassName('site-head')[0].style.display='none'");
-          controller.evaluateJavascript(
-              "document.getElementsByTagName('footer')[0].style.display='none'");
+                  controller.evaluateJavascript(
+                      "document.getElementsById('head')[0].style.display='none';");
+                  controller.evaluateJavascript(
+                      "document.getElementsByClassName('site-head')[0].style.display='none'");
+                  controller.evaluateJavascript(
+                      "document.getElementsByTagName('footer')[0].style.display='none'");
 
-          controller.evaluateJavascript(
-              "document.getElementsByClassName('cont-head')[0].style.display = 'none'");
+                  controller.evaluateJavascript(
+                      "document.getElementsByClassName('cont-head')[0].style.display = 'none'");
 
-          controller.evaluateJavascript(
-              "document.getElementsByClassName('movie-lap01')[0].style.display = 'none'");
+                  controller.evaluateJavascript(
+                      "document.getElementsByClassName('movie-lap01')[0].style.display = 'none'");
 
-          controller.evaluateJavascript(
-              "document.getElementsByClassName('movi-set-wrap')[0].style.display = 'none'");
+                  controller.evaluateJavascript(
+                      "document.getElementsByClassName('movi-set-wrap')[0].style.display = 'none'");
 
-          controller.evaluateJavascript(
-              "document.getElementsByClassName('control-movi-wrap')[0].style.display = 'none'");
+                  controller.evaluateJavascript(
+                      "document.getElementsByClassName('control-movi-wrap')[0].style.display = 'none'");
 
-          controller.evaluateJavascript(
-              "document.getElementsByClassName('footer')[0].style.display = 'none'");
+                  controller.evaluateJavascript(
+                      "document.getElementsByClassName('footer')[0].style.display = 'none'");
 
-          controller.evaluateJavascript(
-              "document.getElementsByClassName('sticky-wrapper')[0].style.display='none';");
-        },
+                  controller.evaluateJavascript(
+                      "document.getElementsByClassName('sticky-wrapper')[0].style.display='none';");
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/weather_image/second_page.dart
+++ lib/weather_image/second_page.dart
@@ -1,7 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-//import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class second_page extends StatefulWidget {
   @override
@@ -11,87 +13,75 @@ class second_page extends StatefulWidget {
 class _second_pageState extends State<second_page> {
   WebViewController controller;
 
-  // InAppWebViewController webView;
+  final String iOSTestUnitId = 'ca-app-pub-3940256099942544/2934735716';
+  final String androidTestUnitId = 'ca-app-pub-3940256099942544/6300978111';
 
-  // @override
-  // void initState() {
-  //   super.initState();
-  // }
-  //
-  // @override
-  // void dispose() {
-  //   super.dispose();
-  // }
+  BannerAd banner;
 
-  // @override
-  // Widget build(BuildContext context) {
-  //   return MaterialApp(
-  //     home: Scaffold(
-  //       body: Container(
-  //           child: Column(children: <Widget>[
-  //             Expanded(
-  //                 child: InAppWebView(
-  //                   initialUrl: "https://www.weather.go.kr/w/image/vshrt/rain.do",
-  //                   initialHeaders: {},
-  //                   initialOptions: InAppWebViewGroupOptions(
-  //                     crossPlatform: InAppWebViewOptions(
-  //                         debuggingEnabled: true,
-  //                         preferredContentMode: UserPreferredContentMode.DESKTOP),
-  //                   ),
-  //                   onWebViewCreated: (InAppWebViewController controller) {
-  //                     webView = controller;
-  //                   },
-  //                   onLoadStart: (InAppWebViewController controller, String url) {
-  //
-  //                   },
-  //                   onLoadStop: (InAppWebViewController controller, String url) async {
-  //
-  //                   },
-  //                 ))
-  //           ])),
-  //     ),
-  //   );
-  // }
-  //
-  //
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    banner = BannerAd(
+      listener: AdListener(),
+      size: AdSize.banner,
+      adUnitId: Platform.isIOS ? iOSTestUnitId : androidTestUnitId,
+      request: AdRequest(),
+    )..load();
+  }
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      body: WebView(
-        //  initialUrl: 'https://www.weather.go.kr/w/image/radar.do',
-        initialUrl: 'https://www.weather.go.kr/wgis-nuri/html/map.html',
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
 
-        // initialUrl: 'http://www.kweather.co.kr/data/RDR/RDR_CMI_202110092300.png',
+            height: 700.0,
+            child:  WebView(
+              //  initialUrl: 'https://www.weather.go.kr/w/image/radar.do',
+              initialUrl: 'https://www.weather.go.kr/wgis-nuri/html/map.html',
 
-        javascriptMode: JavascriptMode.unrestricted,
+              // initialUrl: 'http://www.kweather.co.kr/data/RDR/RDR_CMI_202110092300.png',
 
-        onWebViewCreated: (controller) {
-          this.controller = controller;
+              javascriptMode: JavascriptMode.unrestricted,
 
-          controller.evaluateJavascript(
-              "document.getElementsByTagName('head')[0].style.display='none';");
-          controller.evaluateJavascript(
-              "document.getElementsByClassName('head')[0].style.display='none';");
-          controller.evaluateJavascript(
-              "document.getElementsByTagName('footer')[0].style.display='none'");
+              onWebViewCreated: (controller) {
+                this.controller = controller;
 
-          controller.evaluateJavascript(
-              "document.getElementsByClassName('cont-head')[0].style.display = 'none'");
+                controller.evaluateJavascript(
+                    "document.getElementsByTagName('head')[0].style.display='none';");
+                controller.evaluateJavascript(
+                    "document.getElementsByClassName('head')[0].style.display='none';");
+                controller.evaluateJavascript(
+                    "document.getElementsByTagName('footer')[0].style.display='none'");
 
-          controller.evaluateJavascript(
-              "document.getElementsByClassName('movie-lap01')[0].style.display = 'none'");
+                controller.evaluateJavascript(
+                    "document.getElementsByClassName('cont-head')[0].style.display = 'none'");
 
-          controller.evaluateJavascript(
-              "document.getElementsByClassName('movi-set-wrap')[0].style.display = 'none'");
+                controller.evaluateJavascript(
+                    "document.getElementsByClassName('movie-lap01')[0].style.display = 'none'");
 
-          controller.evaluateJavascript(
-              "document.getElementsByClassName('control-movi-wrap')[0].style.display = 'none'");
+                controller.evaluateJavascript(
+                    "document.getElementsByClassName('movi-set-wrap')[0].style.display = 'none'");
 
-          controller.evaluateJavascript(
-              "document.getElementsByClassName('footer')[0].style.display = 'none'");
-        },
+                controller.evaluateJavascript(
+                    "document.getElementsByClassName('control-movi-wrap')[0].style.display = 'none'");
+
+                controller.evaluateJavascript(
+                    "document.getElementsByClassName('footer')[0].style.display = 'none'");
+              },
+            ),
+
+          ),
+          Container(
+            height: 100.0,
+            child:
+                this.banner == null ? Container() : AdWidget(ad: this.banner),
+          ),
+        ],
       ),
     );
   }
