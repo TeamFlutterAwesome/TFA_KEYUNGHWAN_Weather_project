836e9c50977c10d6c458b12b87f79540b4cde22e
keyunghwan <keyunghwan@gmail.com>
2021-11-09 07:19:12 +0900

    날씨 영상 바텀 부분에 애드몹 넣는 것 성공
    모든 페이지에 애드몹 배치하였으나, 웹뷰가 바로 보이지 않음. 문제 해결해야 함

--- lib/weather_image/second_page.dart
+++ lib/weather_image/second_page.dart
@@ -18,6 +18,7 @@ class _second_pageState extends State<second_page> {
 
   BannerAd banner;
 
+
   @override
   void initState() {
     // TODO: implement initState
@@ -28,60 +29,76 @@ class _second_pageState extends State<second_page> {
       adUnitId: Platform.isIOS ? iOSTestUnitId : androidTestUnitId,
       request: AdRequest(),
     )..load();
+
+    // WebView(initialUrl:
+    // 'https://www.weather.go.kr/wgis-nuri/html/map.html',
+    //   javascriptMode: JavascriptMode.unrestricted,);
+
   }
 
   @override
   Widget build(BuildContext context) {
+    double itemSize;
+    Size size = MediaQuery.of(context).size; // 앱이 구동되는 모바일폰의 화면 사이즈를 가지고 오는 것
+    itemSize = size.width / 3; // 초기값 3
+
     // TODO: implement build
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Container(
+      body: Container(
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                Container(
+                  height: size.height / 1.4,
+                  child: WebView(
+                    //  initialUrl: 'https://www.weather.go.kr/w/image/radar.do',
+                    initialUrl:
+                        'https://www.weather.go.kr/wgis-nuri/html/map.html',
 
-            height: 700.0,
-            child:  WebView(
-              //  initialUrl: 'https://www.weather.go.kr/w/image/radar.do',
-              initialUrl: 'https://www.weather.go.kr/wgis-nuri/html/map.html',
+                    // initialUrl: 'http://www.kweather.co.kr/data/RDR/RDR_CMI_202110092300.png',
 
-              // initialUrl: 'http://www.kweather.co.kr/data/RDR/RDR_CMI_202110092300.png',
+                    javascriptMode: JavascriptMode.unrestricted,
 
-              javascriptMode: JavascriptMode.unrestricted,
+                    onWebViewCreated: (controller) {
+                      this.controller = controller;
 
-              onWebViewCreated: (controller) {
-                this.controller = controller;
+                      controller.evaluateJavascript(
+                          "document.getElementsByTagName('head')[0].style.display='none';");
+                      controller.evaluateJavascript(
+                          "document.getElementsByClassName('head')[0].style.display='none';");
+                      controller.evaluateJavascript(
+                          "document.getElementsByTagName('footer')[0].style.display='none'");
 
-                controller.evaluateJavascript(
-                    "document.getElementsByTagName('head')[0].style.display='none';");
-                controller.evaluateJavascript(
-                    "document.getElementsByClassName('head')[0].style.display='none';");
-                controller.evaluateJavascript(
-                    "document.getElementsByTagName('footer')[0].style.display='none'");
+                      controller.evaluateJavascript(
+                          "document.getElementsByClassName('cont-head')[0].style.display = 'none'");
 
-                controller.evaluateJavascript(
-                    "document.getElementsByClassName('cont-head')[0].style.display = 'none'");
+                      controller.evaluateJavascript(
+                          "document.getElementsByClassName('movie-lap01')[0].style.display = 'none'");
 
-                controller.evaluateJavascript(
-                    "document.getElementsByClassName('movie-lap01')[0].style.display = 'none'");
+                      controller.evaluateJavascript(
+                          "document.getElementsByClassName('movi-set-wrap')[0].style.display = 'none'");
 
-                controller.evaluateJavascript(
-                    "document.getElementsByClassName('movi-set-wrap')[0].style.display = 'none'");
+                      controller.evaluateJavascript(
+                          "document.getElementsByClassName('control-movi-wrap')[0].style.display = 'none'");
 
-                controller.evaluateJavascript(
-                    "document.getElementsByClassName('control-movi-wrap')[0].style.display = 'none'");
-
-                controller.evaluateJavascript(
-                    "document.getElementsByClassName('footer')[0].style.display = 'none'");
-              },
+                      controller.evaluateJavascript(
+                          "document.getElementsByClassName('footer')[0].style.display = 'none'");
+                    },
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(0.0),
+                  color: Colors.white,
+                  height: size.height/10,
+                  child: this.banner == null
+                      ? Container()
+                      : AdWidget(ad: this.banner),
+                ),
+              ],
             ),
-
-          ),
-          Container(
-            height: 100.0,
-            child:
-                this.banner == null ? Container() : AdWidget(ad: this.banner),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
